75abef1ffaa472761d53d54b09d263e9d721c3c1
6431503106 <6431503106@lamduan.mfu.ac.th>
2024-10-25 00:56:56 +0700

    prepraed2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
 import HomeScreen from './screens/HomeScreen.jsx'
 import ProductScreen from './screens/ProductScreen.jsx'
 import store from './store.js'
